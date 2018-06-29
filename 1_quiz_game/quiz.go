a56bd579e085856935e2bce75d8c67e5869c1643
Rosie Hamilton <enchantrix@gmail.com>
2018-06-29 17:58:04 +0100

    reads csv from csv flag

--- 1_quiz_game/quiz.go
+++ 1_quiz_game/quiz.go
@@ -1,36 +1,68 @@
 package main
 
-import "fmt"
 import "flag"
-
-// import "encoding/csv"
+import "encoding/csv"
 import "os"
+import "log"
+import "errors"
+import "fmt"
 
-// global variable for csv flag
-var csv string
+var csvPath string = ""
 
 func main() {
 
-	
-	flag.StringVar(&csv, "csv", "problems.csv", "the name of the csv to use")
+	flag.StringVar(&csvPath, "csv", "problems.csv", "path to csv file")
 	flag.Parse()
 
-	fmt.Println(csv)
+	data, err := readDataFromCsv(csvPath)
+	if err != nil {
+		return
+	}
 
-	csv := getCsvName(os.Args)
-	fmt.Println(csv)
-
-	// open the csv file by default
-	// create a new reader
-	// pass this reader to ther new csv reader method
-	// use read() and test EOF
+	startQuiz(data)
 
 }
 
-func getCsvName(args []string) string {
-	if len(args) == 2 {
-		return args[1]
-	} else {
-		return "problems.csv"
+func readDataFromCsv(path string) ([][]string, error) {
+	if path[len(path)-4:] != ".csv" {
+		errMsg := "Error: Questions and answers must be in a .csv file, received " + path[len(path)-4:]
+		err:= errors.New(errMsg)
+		log.Println(err)
+		return nil, err
+	}
+	file, err := os.Open(path)
+	if err != nil {
+		log.Println(err)
+		return nil, err
+	}
+	defer file.Close()
+	reader := csv.NewReader(file)
+	data, err := reader.ReadAll()
+	if err != nil {
+		log.Println(err)
+	}
+	log.Printf("successfully read %d lines from %s \n", len(data), path)
+	return data, nil
+}
+
+func startQuiz(data [][]string){
+  for i, v := range data{
+		fmt.Printf("Question %d of %d:\n", i+1, len(data))
+		fmt.Println(v[0])
+		a := getAnswer()
+    fmt.Println(a)
 	}
 }
+
+func getAnswer()string{
+	fmt.Printf("answer:" )
+	text := ""
+	fmt.Scanln(&text)
+	return text
+
+}
+
+	
+
+
+
--- 1_quiz_game/quiz_test.go
+++ 1_quiz_game/quiz_test.go
@@ -4,8 +4,6 @@ import "testing"
 import "flag"
 import "os"
 
-//import "fmt"
-
 func TestMain(m *testing.M) {
 
 	flag.Parse()
@@ -14,21 +12,57 @@ func TestMain(m *testing.M) {
 
 }
 
-func TestDefaultCsvNameCorrect(t *testing.T) {
-	args := []string{"./quiz"}
-	result := getCsvName(args)
-	expected := "problems.csv"
-	if result != expected {
-		t.Errorf("unexpected result: was expecting: %s, actual was: %s", expected, result)
+// to do:
+// refactor asserts with stretchr/testify
+// apply table based testing pattern to csv testsg
+
+
+func TestCanReadDataFromValidCsv(t *testing.T) {
+	path := "testdata/test.csv"
+	data, _ := readDataFromCsv(path)
+
+	if len(data) != 2 {
+		t.Errorf("Error was expecting data with length of 2 but length was: %d", len(data))
+	}
+
+	expected_row1 := []string{"1 + 1", "2"}
+	expected_row2 := []string{"Why can't spaghetti code?", "Impasta syndrome"}
+	expected := [][]string{expected_row1, expected_row2}
+
+	for j, row := range data {
+		for i, item := range row {
+			if item != expected[j][i] {
+				t.Errorf("unexpected result: was expecting: %s, actual was: %s", expected[j][i], item)
+			}
+		}
 	}
 }
 
-func TestCsvNameCorrectWhenArgPassed(t *testing.T) {
-	args := []string{"./quiz", "test.csv"}
-	result := getCsvName(args)
-	expected := "test.csv"
-	if result != expected {
-		t.Errorf("unexpected result: was expecting: %s, actual was: %s", expected, result)
+func TestCsvFileIsMissing(t *testing.T) {
+	path := "testdata/missing.csv"
+	data, err := readDataFromCsv(path)
+
+	if len(data) != 0 {
+		t.Errorf("Error was expecting data with length of 0 but length was: %d", len(data))
+	}
+
+	expected := "open testdata/missing.csv: no such file or directory"
+	if err.Error() != expected {
+		t.Errorf("unexpected result: was expecting: %s, actual was: %s", expected, err.Error())
+	}
+}
+
+func TestQuestionsNotInCsvFormat(t *testing.T) {
+	path := "testdata/helloworld.txt"
+	data, err := readDataFromCsv(path)
+
+	if len(data) != 0 {
+		t.Errorf("Error was expecting data with length of 0 but length was: %d", len(data))
+	}
+
+	expected := "Error: Questions and answers must be in a .csv file, received .txt"
+	if err.Error() != expected {
+		t.Errorf("unexpected result: was expecting: %s, actual was: %s", expected, err.Error())
 	}
 }
 
