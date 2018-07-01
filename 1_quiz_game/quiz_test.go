4857ec7aea177439a3df8d1c401d089a1c2f0ae6
Rosie <enchantrix@gmail.com>
2018-07-01 07:05:19 +0100

    test refactoring

--- 1_quiz_game/quiz_test.go
+++ 1_quiz_game/quiz_test.go
@@ -4,7 +4,6 @@ import "testing"
 import "flag"
 import "os"
 import "errors"
-
 import "github.com/stretchr/testify/assert"
 
 func TestMain(m *testing.M) {
@@ -15,29 +14,38 @@ func TestMain(m *testing.M) {
 
 }
 
-// to do refactor length of responses into a test table
-
-func TestReadDataFromCsvErrors(t *testing.T){
-	var tests = []struct{
+func TestReadDataFromCsvErrors(t *testing.T) {
+	var tests = []struct {
 		input string
-		err error 
+		err   error
 	}{
 		{"testdata/test.csv", nil},
 		{"testdata/helloworld.txt", errors.New("Error: Questions and answers must be in a .csv file, received .txt")},
 	}
 	for _, test := range tests {
-		_, e := readDataFromCsv(test.input); 
-		assert.Equal (t, e, test.err, "unexpected error returned")
+		_, e := readDataFromCsv(test.input)
+		assert.Equal(t, e, test.err, "unexpected error returned")
 	}
-} 
+}
+
+func TestReadDataFromCsvReturnsDataOfCorrectLength(t *testing.T) {
+	var tests = []struct {
+		input      string
+		dataLength int
+	}{
+		{"testdata/test.csv", 2},
+		{"testdata/missing.csv", 0},
+		{"testdata/helloworld.txt", 0},
+	}
+	for _, test := range tests {
+		data, _ := readDataFromCsv(test.input)
+		assert.Equal(t, len(data), test.dataLength, "unexpected data returned")
+	}
+}
 
 func TestCanReadDataFromValidCsv(t *testing.T) {
 	path := "testdata/test.csv"
 	data, _ := readDataFromCsv(path)
-
-	if len(data) != 2 {
-		t.Errorf("Error was expecting data with length of 2 but length was: %d", len(data))
-	}
 
 	expected_row1 := []string{"1 + 1", "2"}
 	expected_row2 := []string{"Why can't spaghetti code?", "Impasta syndrome"}
@@ -45,32 +53,15 @@ func TestCanReadDataFromValidCsv(t *testing.T) {
 
 	for j, row := range data {
 		for i, item := range row {
-			assert.Equal (t, expected[j][i], item,  "unexpected values read from CSV")
+			assert.Equal(t, expected[j][i], item, "unexpected values read from CSV")
 		}
 	}
 }
 
 func TestCsvFileIsMissing(t *testing.T) {
 	path := "testdata/missing.csv"
-	data, err := readDataFromCsv(path)
-
-	if len(data) != 0 {
-		t.Errorf("Error was expecting data with length of 0 but length was: %d", len(data))
-	}
+	_, err := readDataFromCsv(path)
 
 	expected := "open testdata/missing.csv: no such file or directory"
-	assert.Equal (t, expected, err.Error(), "unexpected error message")
+	assert.Equal(t, expected, err.Error(), "unexpected error message")
 }
-
-func TestQuestionsNotInCsvFormat(t *testing.T) {
-	path := "testdata/helloworld.txt"
-	data, err := readDataFromCsv(path)
-
-	if len(data) != 0 {
-		t.Errorf("Error was expecting data with length of 0 but length was: %d", len(data))
-	}
-
-	expected := "Error: Questions and answers must be in a .csv file, received .txt"
-	assert.Equal (t, expected, err.Error(), "unexpected error message")
-}
-
